c6c30d5a43050d46b0ea58cbee1258e5c91ebf92
victorarmisen <victorarmisencapo@enti.cat>
2020-01-09 23:29:57 +0100

    Twitch Api Response

--- A_VictorArmisen/app/src/main/java/com/example/a_victorarmisen/fragment/StreamsFragment.kt
+++ A_VictorArmisen/app/src/main/java/com/example/a_victorarmisen/fragment/StreamsFragment.kt
@@ -1,19 +1,24 @@
 package com.example.a_victorarmisen.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.a_victorarmisen.R
 import com.example.a_victorarmisen.network.TwitchApiService
+import okhttp3.Call
+import okhttp3.Response
+import java.io.IOException
+import javax.security.auth.callback.Callback
 
 class StreamsFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_streams, container, false)
     }
@@ -23,8 +28,27 @@ class StreamsFragment : Fragment() {
         //Code
 
         //TwitchApiService.endpoints.getStreams();
+        TwitchApiService.endpoints.getStreams().enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: Call, e: IOException) {
 
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("StreamsFragment", "++ onResponse ++ ")
+                if(response.code() == 200) {
+                    //All good
+                    Log.i("StreamsFragment", response.body()?.string() ?: "Null body")
+                } else {
+                    //Problem
+                    Log.w("StreamsFragment", response.message())
+                }
+                response.body()
+            }
+
+
+        })
     }
 
-
 }
+
+
--- A_VictorArmisen/app/src/main/java/com/example/a_victorarmisen/network/TwitchApiService.kt
+++ A_VictorArmisen/app/src/main/java/com/example/a_victorarmisen/network/TwitchApiService.kt
@@ -1,24 +1,27 @@
 package com.example.a_victorarmisen.network
 
+import okhttp3.Call
 import retrofit2.Retrofit
-
+import retrofit2.http.GET
+import retrofit2.http.Headers
 
 
 interface TwitchApiService {
 
-    fun getStreams() {
-
-    }
+    @Headers("Client-ID: ywvglt0gib8rqdly0ejobehqfi071m" )
+    @GET("/streams")
+    fun getStreams() : Call
 
 
     //Create https client
     companion object {
         private val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.twitch.tv/helix")
-            .build()
+                .baseUrl("https://api.twitch.tv/helix")
+                .build()
 
-        val endpoints = retrofit.create<TwitchApiService>(TwitchApiService::class.java!!)
+        val endpoints = retrofit.create<TwitchApiService>(TwitchApiService::class.java)
     }
+
 
 
 }
