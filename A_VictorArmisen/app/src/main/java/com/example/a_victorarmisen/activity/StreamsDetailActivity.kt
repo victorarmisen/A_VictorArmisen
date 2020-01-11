5cc8c07a66cac21641cce55369004520150e4b3d
victorarmisen <victorarmisencapo@enti.cat>
2020-01-11 22:58:19 +0100

    message

--- A_VictorArmisen/app/src/main/java/com/example/a_victorarmisen/activity/StreamsDetailActivity.kt
+++ A_VictorArmisen/app/src/main/java/com/example/a_victorarmisen/activity/StreamsDetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.a_victorarmisen.activity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +15,9 @@ public class StreamsDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_streamsdetail)
 
         val stream_name_textView = findViewById<TextView>(R.id.stream_name) as TextView;
-        val get_intent_name = getIntent().getStringExtra("stream_name")
+        val get_intent_name = intent.getStringExtra("name_stream")
+
+        Log.i("StreamsDetailActivity",get_intent_name);
 
         stream_name_textView.setText(get_intent_name)
 
